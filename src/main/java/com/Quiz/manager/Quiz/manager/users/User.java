ffd5fa09c399a267ae99014e0b22c23ba4dd8229
Kally95 <kally-95@hotmail.com>
2022-10-17 14:22:39 +0100

    Added service layer and repo to extra mockaroo data from db

--- src/main/java/com/Quiz/manager/Quiz/manager/users/User.java
+++ src/main/java/com/Quiz/manager/Quiz/manager/users/User.java
@@ -1,6 +1,9 @@
 package com.Quiz.manager.Quiz.manager.users;
 
+import com.sun.istack.NotNull;
 import lombok.*;
+
+import javax.persistence.*;
 
 @ToString
 @Getter
@@ -8,10 +11,36 @@ import lombok.*;
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            generator = "student_sequence",
+            strategy = GenerationType.SEQUENCE)
     private Long id;
+
+    @Column(nullable = false)
     private String username;
-    private String email;
+
     private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Permission permission;
+    public User(String username, String email, String password, Permission permission) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.permission = permission;
+    }
 }
--- src/main/java/com/Quiz/manager/Quiz/manager/users/UserController.java
+++ src/main/java/com/Quiz/manager/Quiz/manager/users/UserController.java
@@ -1,21 +1,19 @@
 package com.Quiz.manager.Quiz.manager.users;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/users")
+@AllArgsConstructor
 public class UserController {
-
+    private final UserService userService;
     @GetMapping
     List<User> getUsers() {
-        return Arrays.asList(
-                new User(1L, "Ali", "Ali@java.com", "password123", Permission.EDIT),
-                new User(2L, "Simon", "Simon@java.com", "p@ssword!", Permission.RESTRICTED)
-        );
+        return userService.getAllUsers();
     }
 }
